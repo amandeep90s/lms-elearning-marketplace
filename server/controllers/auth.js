76f187b7e88646cc45f5bdc4755cc6dfab7507e5
Amandeep Singh <programmeramandeep@gmail.com>
2022-05-26 22:16:10 +0530

    Setup controller

---
+++ server/controllers/auth.js
@@ -0,0 +1,4 @@
+// Register user controller
+export const register = (req, res) => {
+  res.send("Hello aman!");
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,10 @@
-import express from "express";
+const express = require("express");
 
 const router = express.Router();
 
-router.get("/register", (_, res) => {
-  res.send("register user");
-});
+// Auth controller
+const { register } = require("../controllers/auth");
+
+router.get("/register", register);
 
 module.exports = router;
